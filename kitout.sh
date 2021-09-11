433a99bef44ad1127ce6393455c119d825554daa
Mark Norman Francis <norm@201created.com>
2021-09-11 09:18:04 +0100

    Add help and version options/arguments

--- kitout.sh
+++ kitout.sh
@@ -1,17 +1,46 @@
 #!/usr/bin/env -S bash -euo pipefail
 
+VERSION=0.1
+
 # ANSI sequences
 bold="\e[1m"
 magenta="\e[35m"
 reset="\e[0m"
 
 function main {
-    error "Kitout does nothing, yet."
-    exit 1
+    while getopts "hv" option; do
+        case $option in
+            v)      show_version ;;
+            ?|h)    usage ;;
+        esac
+    done
+    shift $(( OPTIND-1 ))
+
+    [ -z "${1:-}" ] && usage 1
+    [ "${1}" = 'help' ] && usage
+    [ "${1}" = 'version' ] && show_version
+}
+
+function usage {
+    cat << EOF | sed -e 's/^        //'
+        Usage:
+
+        kitout [-h|help]
+            show this usage information
+
+        kitout -v|version
+            show the version number
+EOF
+    exit "${1:-0}"
+}
+
+function show_version {
+    echo kitout version $VERSION
+    exit 0
 }
 
 function error {
     printf "${bold}${magenta}*** ${1}${reset}\n" >&2
 }
 
-main
+main "$@"
--- tests/00.executable.bats
+++ tests/00.executable.bats
@@ -1,13 +1,37 @@
 #!/usr/bin/env bash
 
-bold=$'\e'[1m
-magenta=$'\e'[35m
-reset=$'\e'[0m
-
 @test kitout_is_executable {
     run ./kitout.sh
     echo "$output"
 
-    [ "${lines[0]}" == "${bold}${magenta}*** Kitout does nothing, yet.${reset}" ]
+    [ "${lines[0]}" == "Usage:" ]
     [ $status -eq 1 ]
+}
+
+@test kitout_explicit_help_not_error {
+    run ./kitout.sh -h
+    echo "$output"
+
+    [ "${lines[0]}" == "Usage:" ]
+    [ $status -eq 0 ]
+
+    run ./kitout.sh help
+    echo "$output"
+
+    [ "${lines[0]}" == "Usage:" ]
+    [ $status -eq 0 ]
+}
+
+@test kitout_shows_version {
+    run ./kitout.sh -v
+    echo "$output"
+
+    [ "${lines[0]}" == "kitout version 0.1" ]
+    [ $status -eq 0 ]
+
+    run ./kitout.sh version
+    echo "$output"
+
+    [ "${lines[0]}" == "kitout version 0.1" ]
+    [ $status -eq 0 ]
 }
